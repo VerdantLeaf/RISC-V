779d19c7b7e34e4ec2588fbeade7fa3a637f4a84
VerdantLeaf <111800371+VerdantLeaf@users.noreply.github.com>
2025-07-22 10:33:35 -0400

    EX/MEM register
    Completed EX/MEM register.

--- vivadoSrc/rtl/ex_mem_reg.v
+++ vivadoSrc/rtl/ex_mem_reg.v
@@ -31,24 +31,59 @@ module ex_mem_reg #(
  
     input clk,
     input rst,
-    // Data
-    input [31:0] branch_target,     // PC of branch address
-    input [31:0] rdata2,            // Data written to memory
-    
-    // Ctrl signals
-    input [4:0] rd,                 // Select register to write to
-    input en_write_reg,             // enable register writing in regfile
 
-    input alu_zero,                 // is alu result zero
-    input alu_pos,                  // is alu result pos (if not pos and not zero, then neg)
+    input [ADDR_SIZE - 1 : 0] branch_target,    // PC of branch target
+    input [WORD_SIZE - 1 : 0] alu_result,       // The result of the ALU operation
+    input [WORD_SIZE - 1 : 0] write_data,       // Data written to memory
+    input [REG_SEL - 1 : 0] rd,   // Select register to write to
+
+    input alu_zero,             // is alu result zero (if it is, we branch)
+    input branch,               // is instruction a branch instruction
+    input jump                  // is instruction a jump instruction
+    input mem_read,             // enable mem read
+    input mem_write,            // enable mem write
+    input reg_write,            // enable write back for memory
     
-    input branch,                   // is operation a branch operation
-    input mem_read,                 // enable mem read
-    input mem_write,                // enable mem write
-    input write_source              // Select write from data memory or ALU result
-    
+
+    output [ADDR_SIZE - 1 : 0] branch_target_out,    
+    output [WORD_SIZE - 1 : 0] alu_result_out,       
+    output [WORD_SIZE - 1 : 0] write_data_out,       
+    output [REG_SEL - 1 : 0] rd_out,  
+
+    output alu_zero_out,             
+    output branch_out,   
+    output jump_out,            
+    output mem_read_out,             
+    output mem_write_out,            
+    output reg_write_out            
+
     );
     
-    
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            branch_target_out <= {ADDR_SIZE{1'b0}};
+            alu_result_out <= {WORD_SIZE{1'b0}};
+            write_data_out <= {WORD_SIZE{1'b0}};
+            rd_out <= {REG_SEL{1'b0}};
 
+            alu_zero_out <= 1'b0; 
+            branch_out <= 1'b0;
+            jump_out <= 1'b0;
+            mem_read_out <= 1'b0;
+            mem_write_out <= 1'b0;
+            reg_write_out <= 1'b0;
+        end else begin
+            branch_target_out <= branch_target;
+            alu_result_out <= alu_result;
+            write_data_out <= write_data;
+            rd_out <= rd;
+
+            alu_zero_out <= alu_zero; 
+            branch_out <= branch;
+            jump_out <= jump;
+            mem_read_out <= mem_read;
+            mem_write_out <= mem_write;
+            reg_write_out <= reg_write;
+        end
+    end
 endmodule
